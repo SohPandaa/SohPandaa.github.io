e559a5e5c826ef6ceaccd1f82109b13ee16451c4
SohPandaa <40484155+SohPandaa@users.noreply.github.com>
2018-06-22 19:37:50 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+window.addEventListener("keydown", function(e) {
+    // space, page up, page down and arrow keys:
+    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 context.scale(20, 20);
 
 function arenaSweep() {
